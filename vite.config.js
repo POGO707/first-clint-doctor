bad3b00d6b44a8ad00837b393c4415c2acb19ba4
POGO707 <hmoinuddin84@gmail.com>
2025-11-25 11:04:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
-});
+})
